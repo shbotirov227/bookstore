97fcf2fbcbe4e81cad5a90ef8a92e2089da996e6
Shohruh174 <shbotirov227@gmail.com>
2023-01-31 08:45:13 +0500

    n

---
+++ src/books.js
@@ -0,0 +1,44 @@
+const BOOKS = [
+    {
+      id: 1,
+      image: book1,
+      title: 'The Shadow Sister',
+      author: 'Lucinda Riley',
+      price: '$40'
+    },
+    {
+      id: 2,
+      image: book2,
+      title: 'Jade War',
+      author: 'Fonda Lee',
+      price: '$70.99'
+    },
+    {
+      id: 3,
+      image: book3,
+      title: 'Call Me By Your Name',
+      author: 'Andre Aciman',
+      price: '$58'
+    },
+    {
+      id: 4,
+      image: book4,
+      title: 'The Dinner Quest',
+      author: 'B.P. Walter',
+      price: '$198'
+    },
+    {
+      id: 5,
+      image: book5,
+      title: 'Marauder',
+      author: 'Clive Cussler and Boyd Morrison',
+      price: '$34.55'
+    },
+    {
+      id: 6,
+      image: book6,
+      title: 'The Silmarillion',
+      author: 'L.R.R Tolkien',
+      price: '$44.84'
+    },
+  ]
